33bf16c8af48e53ae857904dd4437f3624adf043
leaptrotaj <49792990+leaptrotaj@users.noreply.github.com>
2025-12-03 15:43:32 -0500

    Refactor shiftingtiles plugin for improved performance
    - Updated to match modern practices
    - Changed to have dynamic display size
    - Added jitter to photo timer

--- shiftingtiles.js
+++ shiftingtiles.js
@@ -1,42 +1,42 @@
-(function( $ ){
+(function($){
 
   $.fn.shiftingtiles = function(images, options) {
-  	
-    // Variables
-    var options = $.extend({
+    
+    // Merge options safely
+    var settings = $.extend({
           photosource: images,
           duration: 5000,
+          jitterPercent: 0
         }, options), 
         timeout, 
         where = this;
     
+    // Animation end handlers
     where.on("animationend webkitAnimationEnd oAnimationEnd", ".leave > .row", function(){
       $(this).parent().addClass("left").removeClass("leave");
     });
     where.on("animationend webkitAnimationEnd oAnimationEnd", ".disappear", function(){
-      //console.log("Animation end, removing node",this);
-      // $(this).  this wasn't working, why? no idea
       $(this).css("display", "none").remove();
       where.trigger("st-animate-after");
-
       return false;
     });
 
-    setup(this);
+    setup(where);
 
-    // Source
+    // Source image selector
     function source(){
-      if(typeof images.bottom == "undefined")
+      if (!images || images.length === 0) return null;
+
+      if(typeof images.bottom === "undefined")
         images.bottom = 0;
 
       var index = images.bottom + Math.floor((images.length - images.bottom) * Math.random());
-      //console.log(index, 1, images.bottom, images);
       var one = images.splice(index, 1)[0];
       images.unshift(one);
 
       images.bottom++;
 
-      if(images.bottom == images.length){
+      if(images.bottom === images.length){
         images.bottom = 0;
         where.trigger("st-galleryrestart");
       }
@@ -44,86 +44,91 @@
       return one;
     }
 
-  	// Setup
-  	function setup(where){
-  	 	where.addClass("shiftingtiles");
-	  	where.prepend("<div class='row'><div class='single'></div><div class='single'></div><div class='dual'><div></div><div></div></div></div>");
-	  	where.prepend("<div class='row'><div class='single'></div><div class='single'></div><div class='dual'><div></div><div></div></div></div>");
+    // Setup DOM structure
+    function setup(where){
+      where.addClass("shiftingtiles");
+      where.prepend("<div class='row'><div class='single'></div><div class='single'></div><div class='dual'><div></div><div></div></div></div>");
+      where.prepend("<div class='row'><div class='single'></div><div class='single'></div><div class='dual'><div></div><div></div></div></div>");
       where.append("<div class='loading'>Loading Photos...</div>");
 
       where.find(".single, .dual > div").each(addImage);
-      timeout = setTimeout(frame, options.duration);
+      timeout = setTimeout(frame, settings.duration);
     }
 
-    // Add background image from source to jQuery element
+    // Add background image
     function image($element){
-      $element.css("background-image", "url("+source()+")");
+      var src = source();
+      if(src) $element.css("background-image", "url("+src+")");
     }
 
-    // Figure out single or dual and add images
+    // Add images to nodes
     function addImage(index, node){
       node = $(node);
-      //console.log(node);
-      // Load new images
-      if(node.hasClass("single") || node.parent(".dual").size() > 0){
+      if(node.hasClass("single") || node.parent(".dual").length > 0){
         image(node);
       } else if(node.hasClass("dual")) {
         node.children().each(function(){
           image($(this));
         });
       }
-
       return node;
     }
 
-    // Animate frame, remove box and add new one
-  	function frame(){
-  		clearTimeout(timeout);
-  		var boxes = where.find(".single:not(:last-child), .dual:not(:last-child)");
-  		var disappear = $(boxes.get( ~~(Math.random() * boxes.size()) ));
+    // Animate frame
+    function frame(){
+      clearTimeout(timeout);
+      var boxes = where.find(".single:not(:last-child), .dual:not(:last-child)");
+      if(boxes.length === 0) return;
+
+      var disappear = $(boxes.get(Math.floor(Math.random() * boxes.length)));
 
       where.trigger("st-animate-before", disappear);
 
-  		disappear.parent().append(addImage(0, disappear.clone()));
-  		disappear.addClass("disappear");
+      disappear.parent().append(addImage(0, disappear.clone()));
+      disappear.addClass("disappear");
       where.trigger("st-animate", disappear);
 
-      timeout = setTimeout(frame, options.duration);
-  	}
+      // Jitter logic
+      var jitter = Math.floor(settings.duration * settings.jitterPercent * (Math.random() - 0.5));
+      var nextInterval = settings.duration + jitter;
+      console.log("Next frame in:", nextInterval, "ms");
+      timeout = setTimeout(frame, nextInterval);
+    }
 
-  	$(document.body).keydown(function(e){
-      //console.log("Key up");
-  		if(e.keyCode == 32){ 
+    // Keyboard controls
+    $(document.body).keydown(function(e){
+      if(e.key === 32){ 
         frame();
         e.preventDefault();
         return false;
       }
-      if(e.keyCode == 38){
+      if(e.key === 38){
         $(".shiftingtiles").toggleClass("leave");
       }
-  	});
+    });
     
     // Chainability
     return this;
 
   };
-})( jQuery );
+})(jQuery);
 
+// Polyfill for Array.reduce
 if (!Array.prototype.reduce) {
   Array.prototype.reduce = function reduce(accumulator){
     if (this===null || this===undefined) throw new TypeError("Object is null or undefined");
-    var i = 0, l = this.length >> 0, curr;
+    var i = 0, l = this.length >>> 0, curr;
  
-    if(typeof accumulator !== "function") // ES5 : "If IsCallable(callbackfn) is false, throw a TypeError exception."
+    if(typeof accumulator !== "function")
       throw new TypeError("First argument is not callable");
  
     if(arguments.length < 2) {
       if (l === 0) throw new TypeError("Array length is 0 and no second argument");
       curr = this[0];
-      i = 1; // start accumulating at the second element
-    }
-    else
+      i = 1;
+    } else {
       curr = arguments[1];
+    }
  
     while (i < l) {
       if(i in this) curr = accumulator.call(undefined, curr, this[i], i, this);
